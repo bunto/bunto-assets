ba1df8a5356d32a0768b1f9fc8019353cfe5dd73
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-03-30 16:47:41 +0200

    Create site.rb

---
+++ lib/bunto/assets/patches/bunto/site.rb
@@ -0,0 +1,11 @@
+# ----------------------------------------------------------------------------
+# Frozen-string-literal: true
+# Copyright: 2016-present - MIT License
+# Encoding: utf-8
+# ----------------------------------------------------------------------------
+
+module Bunto
+  class Site
+    attr_accessor :sprockets
+  end
+end
