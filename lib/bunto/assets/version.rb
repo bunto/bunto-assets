7f4c0a9a561f3bb787f9274e7fcfa5260a1f24bf
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-03-30 15:50:52 +0200

    Create version.rb

---
+++ lib/bunto/assets/version.rb
@@ -0,0 +1,11 @@
+# ----------------------------------------------------------------------------
+# Frozen-string-literal: true
+# Copyright: 2016-present - MIT License
+# Encoding: utf-8
+# ----------------------------------------------------------------------------
+
+module Bunto
+  module Assets
+    VERSION="1.0.0.pre.alpha"
+  end
+end
