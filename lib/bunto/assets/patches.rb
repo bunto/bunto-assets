bc5f21a7496ac8acae02dfa852f16db958e24f7a
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-03-30 15:50:11 +0200

    Create patches.rb

---
+++ lib/bunto/assets/patches.rb
@@ -0,0 +1,10 @@
+# ----------------------------------------------------------------------------
+# Frozen-string-literal: true
+# Copyright: 2016-present - MIT License
+# Encoding: utf-8
+# ----------------------------------------------------------------------------
+
+require_relative "patches/bunto/cleaner"
+require_relative "patches/sprockets/asset"
+require_relative "patches/bunto/site"
+require_relative "patches/kernel"
