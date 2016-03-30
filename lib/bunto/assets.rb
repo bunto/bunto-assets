c1be42beb3cffc473fb89d50240d6ab4d3792d98
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-03-30 15:44:36 +0200

    Create assets.rb

---
+++ lib/bunto/assets.rb
@@ -0,0 +1,24 @@
+# ----------------------------------------------------------------------------
+# Frozen-string-literal: true
+# Copyright: 2016-present - MIT License
+# Encoding: utf-8
+# ----------------------------------------------------------------------------
+
+require "sprockets"
+require "bunto"
+
+require_relative "assets/env"
+require_relative "assets/version"
+require_relative "assets/patches"
+require_relative "assets/config"
+require_relative "assets/cached"
+require_relative "assets/hook"
+require_relative "assets/logger"
+require_relative "assets/hooks"
+require_relative "assets/liquid"
+require_relative "assets/addons/bootstrap"
+require_relative "assets/addons/autoprefixer"
+require_relative "assets/addons/font_awesome"
+require_relative "assets/addons/javascript"
+require_relative "assets/processors/liquid"
+require_relative "assets/proxies/magick"
