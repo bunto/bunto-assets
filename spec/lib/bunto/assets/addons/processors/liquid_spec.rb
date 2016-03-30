92db8884385f0dc81ee96d31cd19b4bd9f8be360
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-03-30 17:44:18 +0200

    Create liquid_spec.rb

---
+++ spec/lib/bunto/assets/addons/processors/liquid_spec.rb
@@ -0,0 +1,60 @@
+# Frozen-string-literal: true
+# Copyright: 2016-present - MIT License
+# Encoding: utf-8
+
+require "rspec/helper"
+describe Bunto::Assets::Processors::Liquid do
+  context "when the extension is .liquid" do
+    let(:asset) { env.find_asset("liquid") }
+    let(:env) { Bunto::Assets::Env.new(stub_bunto_site) }
+    let(:source) { asset.to_s }
+
+    it "lets the user use Liquid" do
+      expect(source).not_to match(/\{{2}\s*site\.background_image\s*\}{2}/)
+      expect(source).to match(/background:\s*url\("\/assets\/ruby\.png"\)/)
+    end
+
+    it "provides the right extension" do
+      expect(asset.logical_path).to eq "liquid.css"
+    end
+  end
+
+  context "when the extension is not liquid" do
+    context "and liquid is disabled" do
+      let(:asset) { env.find_asset("noliquid") }
+      let(:env) { Bunto::Assets::Env.new(stub_bunto_site) }
+      let(:source) { asset.to_s }
+
+      it "should not process the liquid" do
+        expect(source).to eq ".background {\n  background: " \
+          "url(\"{{ site.background_image }}\"); }\n"
+      end
+    end
+
+    context "and liquid is enabled" do
+      let(:asset) { env.find_asset("otherliquid") }
+      let(:env) { Bunto::Assets::Env.new(stub_bunto_site("assets" => { "features" => { "liquid" => true }})) }
+      let(:source) { asset.to_s }
+
+      it "should not process the liquid" do
+        expect(source).not_to match(/\{{2}\s*site\.background_image\s*\}{2}/)
+        expect(source).to match(/background:\s*url\("\/assets\/ruby\.png"\)/)
+       end
+    end
+  end
+
+  context "when sprockets requires a liquid file" do
+    let(:asset) { env.find_asset("index") }
+    let(:env) { Bunto::Assets::Env.new(stub_bunto_site) }
+    let(:source) { asset.to_s }
+
+    it "lets the user use Liquid" do
+      expect(source).not_to match(/\{{2}\s*site\.background_image\s*\}{2}/)
+      expect(source).to match(/background:\s*url\("\/assets\/ruby\.png"\)/)
+    end
+
+    it "provides the right extension" do
+      expect(asset.logical_path).to eq "index.css"
+    end
+  end
+end
