0386900325c540a96334fef189680446a0a91efb
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-03-30 15:49:02 +0200

    Create liquid.rb

---
+++ lib/bunto/assets/liquid.rb
@@ -0,0 +1,15 @@
+# ----------------------------------------------------------------------------
+# Frozen-string-literal: true
+# Copyright: 2016-present - MIT License
+# Encoding: utf-8
+# ----------------------------------------------------------------------------
+
+module Bunto
+  module Assets
+    module Liquid
+      require_relative "liquid/drop"
+      require_relative "liquid/filters"
+      require_relative "liquid/tag"
+    end
+  end
+end
