625667cb0f23a0fde550cb87585f0ae9926a1094
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-03-30 17:31:43 +0200

    Create image_spec.rb

---
+++ spec/lib/bunto/assets/liquid/tag/defaults/image_spec.rb
@@ -0,0 +1,58 @@
+# Frozen-string-literal: true
+# Copyright: 2016-present - MIT License
+# Encoding: utf-8
+
+require "rspec/helper"
+require "nokogiri"
+
+describe Bunto::Assets::Liquid::Tag::Defaults::Image do
+  subject { described_class }
+  before :all do
+    @site = stub_bunto_site
+    @env = Bunto::Assets::Env.new(@site)
+    @renderer = @site.liquid_renderer
+    @register = {
+      :registers => {
+        :site => @site
+      }
+    }
+  end
+
+  def stub_tag(tag, data)
+    payload = "{%% %s %s %%}" % [tag, data]
+    fragment(@renderer.file(__FILE__).parse(payload).render!( \
+      @site.site_payload, @register))
+  end
+
+  def fragment(html)
+    Nokogiri::HTML.fragment(html).children. \
+      first
+  end
+
+  it "adds a default alt attribute to img" do
+    expect(stub_tag("img", "ruby.png").attr("alt")).to eq "ruby.png"
+  end
+
+  it "sets a default width" do
+    expect(stub_tag("img", "ruby.png").attr("width")).to( \
+      match(/\A\d+\Z/))
+  end
+
+  it "sets a default height" do
+    expect(stub_tag("img", "ruby.png").attr("height")).to( \
+      match(/\A\d+\Z/))
+  end
+
+  it "allows the user to disable width and height" do
+    stub_asset_config "features" => { "automatic_img_size" => false }
+    result = stub_tag("img", "ruby.png")
+    expect(result.attr( "width")).to(be_nil)
+    expect(result.attr("height")).to(be_nil)
+  end
+
+  it "allows the user to disable alt" do
+    stub_asset_config "features" => { "automatic_img_alt" => false }
+    result = stub_tag("img", "ruby.png")
+    expect(result.attr("alt")).to(be_nil)
+  end
+end
