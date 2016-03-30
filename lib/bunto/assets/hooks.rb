b7333839248957a8551ea4d6d4c650e3c4bd912a
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-03-30 15:48:28 +0200

    Create hooks.rb

---
+++ lib/bunto/assets/hooks.rb
@@ -0,0 +1,20 @@
+# ----------------------------------------------------------------------------
+# Frozen-string-literal: true
+# Copyright: 2016-present - MIT License
+# Encoding: utf-8
+# ----------------------------------------------------------------------------
+
+require_relative "hooks/bunto/drops"
+require_relative "hooks/bunto/setup"
+require_relative "hooks/bunto/write"
+
+require_relative "hooks/context"
+require_relative "hooks/compression"
+require_relative "hooks/configuration"
+require_relative "hooks/helpers"
+require_relative "hooks/logger"
+require_relative "hooks/sources"
+require_relative "hooks/sprockets"
+require_relative "hooks/version"
+require_relative "hooks/cache"
+require_relative "hooks/erb"
